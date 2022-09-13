35d67220f6a738887f5643d8abbcf2f81e25bc78
sinh_bui <sinh.bui@hellohealthgroup.com>
2022-09-13 07:42:44 +0700

    header design and apply for desktop

---
+++ components/common/header/header-desktop.tsx
@@ -0,0 +1,24 @@
+import { Container, Stack, Link as MuiLink } from '@mui/material'
+import { Box } from '@mui/system'
+import Link from 'next/link';
+import * as React from 'react'
+import { ROUTE_LIST } from './routes';
+
+export interface HeaderDesktopProps {
+}
+
+export function HeaderDesktop (props: HeaderDesktopProps) {
+  return (
+    <Box display={{xs: 'none', md: 'block'}} py={2}>
+      <Container>
+        <Stack direction="row" justifyContent="flex-end">
+          {ROUTE_LIST.map(route => (
+            <Link key={route.path} href={route.path} passHref>
+              <MuiLink sx={{ ml: 2 }} underline="hover">{route.label}</MuiLink>
+            </Link>
+          ))}
+        </Stack>
+      </Container>
+    </Box>
+  );
+}
